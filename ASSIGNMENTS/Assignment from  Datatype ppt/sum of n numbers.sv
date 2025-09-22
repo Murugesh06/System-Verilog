cae0ad1b42d7abc826878313633a24334ff7187b
Murugesh06 <murugeshrajrp@gmail.com>
2025-09-22 21:22:30 +0530

    Create sum of n numbers.sv

---
+++ ASSIGNMENTS/Assignment from  Datatype ppt/sum of n numbers.sv
@@ -0,0 +1,20 @@
+module tb;
+  int arr[];
+  int result;
+  
+  initial begin
+   arr=new[5];
+    arr='{2,2,2,2,2};
+    result=sum_of_n_nos(arr);
+    $display("sum=%0d",result);
+  end
+  endmodule
+
+  function sum_of_n_nos(int data[]);
+    static int sum=0;
+    foreach(data[i])begin
+      sum=sum+data[i];
+    end
+    return sum;
+  endfunction
+
