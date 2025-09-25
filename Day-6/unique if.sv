18e109cbbc66c9b16dc8535253c5066aa039f053
Murugesh06 <murugeshrajrp@gmail.com>
2025-09-25 10:12:03 +0530

    Create unique if.sv

---
+++ Day-6/unique if.sv
@@ -0,0 +1,16 @@
+module tb;
+  int num=4;
+  initial begin
+    unique if(num==3)
+      $display("num=3");
+    else if(num==5)
+      $display("num=5");
+    else
+      $display("num is neither 3 nor 5");
+  
+    unique if(num==3)
+      $display("num=3");
+    else if(num==5)
+      $display("num=5");
+  end
+endmodule
