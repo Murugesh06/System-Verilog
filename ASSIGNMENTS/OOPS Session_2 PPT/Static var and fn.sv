89d7cdc25f20843cc5794617c6ea17d5fb40d5ef
Murugesh06 <murugeshrajrp@gmail.com>
2025-10-10 12:37:25 +0530

    Update Static var and fn.sv

--- ASSIGNMENTS/OOPS Session_2 PPT/Static var and fn.sv
+++ ASSIGNMENTS/OOPS Session_2 PPT/Static var and fn.sv
@@ -19,3 +19,26 @@ module tb;
   end
 endmodule
     
+//Invoke static function for 'N' number of times and finally disply the value of static variable. 
+class H;
+ static int count=0;
+  function void increment();
+    count++;
+  endfunction
+  static function display();
+    $display("count after incrementing=%0d",count);
+  endfunction
+endclass
+
+module tb;
+  initial begin
+    int n=5;
+    H obj=new();
+    $display("initial count=%0d",obj.count);
+    for(int i=0;i<n;i++)begin
+      obj.increment();
+    end
+    obj.display();
+  end
+endmodule
+    
