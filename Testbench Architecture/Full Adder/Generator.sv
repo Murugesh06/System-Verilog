4f4def2a27cc723398eb294963cd81fa3a3a7b5b
Murugesh06 <murugeshrajrp@gmail.com>
2025-10-13 14:52:32 +0530

    Create Generator.sv

---
+++ Testbench Architecture/Full Adder/Generator.sv
@@ -0,0 +1,19 @@
+class Generator;
+  transaction trans;
+  mailbox gen2drv;
+  
+  function new(mailbox gen2drv);
+    this.gen2drv=gen2drv;
+  endfunction
+  
+  task main();
+    repeat(2)
+      begin
+        trans=new();
+        trans.randomize;
+        trans.display("Generator Class Signals");
+        gen2drv.put(trans);
+      end
+  endtask
+  
+endclass
