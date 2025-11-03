87b3b863b6c29c2140fa18309067c973f3622482
Murugesh06 <murugeshrajrp@gmail.com>
2025-11-03 11:31:34 +0530

    Create 5.Fork join any.sv

---
+++ Day-12/Threads/5.Fork join any.sv
@@ -0,0 +1,13 @@
+module tb;
+	initial begin
+      $display ("[%0t] Main Thread: Fork join going to start", $time);
+	fork
+      fork
+          #20 $display ("[%0t] Thread1 has finished", $time);
+          #30 $display ("[%0t] Thread3 has finished", $time);
+      join_any
+          #10  $display ("[%0t] Thread2 has finished", $time);
+        join_any
+      $display ("[%0t] Main Thread: Fork join has finished", $time);
+	end
+endmodule
