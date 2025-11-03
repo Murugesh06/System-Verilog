5a4197c02fc286c85ad4497c202606fcbc4b7454
Murugesh06 <murugeshrajrp@gmail.com>
2025-11-03 12:24:42 +0530

    Create 8.Disable Fork.sv

---
+++ Day-12/Threads/8.Disable Fork.sv
@@ -0,0 +1,12 @@
+module tb;
+	initial begin
+      $display ("[%0t] Main Thread: Fork join going to start", $time);
+		fork
+          #20 $display ("[%0t] Thread1 has finished", $time);
+          #30 $display ("[%0t] Thread3 has finished", $time);
+          #10  $display ("[%0t] Thread2 has finished", $time);
+        join_any
+      $display ("[%0t] Main Thread: Fork join has finished", $time);
+      disable fork;
+	end
+endmodule
