82cb15583a8597cabeac6b104505f13d1d0c59a4
Murugesh06 <murugeshrajrp@gmail.com>
2025-11-03 12:02:06 +0530

    Create 7.Nested Fork Join None.sv

---
+++ Day-12/Threads/7.Nested Fork Join None.sv
@@ -0,0 +1,14 @@
+module tb;
+	initial begin
+      $display ("[%0t] Main Thread: Fork join going to start", $time);
+	 fork
+       $display("[%0t] Outer fork",$time);
+      fork
+          #20 $display ("[%0t] Thread1 has finished", $time);
+          #30 $display ("[%0t] Thread3 has finished", $time);
+      join_none
+          #10  $display ("[%0t] Thread2 has finished", $time);
+     join_none
+      $display ("[%0t] Main Thread: Fork join has finished", $time);
+	end
+endmodule
