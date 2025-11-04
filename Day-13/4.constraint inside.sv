0b4dc998b2fc07d269751d740399d469fbb8fb29
Murugesh06 <murugeshrajrp@gmail.com>
2025-11-04 10:54:49 +0530

    Create 4.constraint inside.sv

---
+++ Day-13/4.constraint inside.sv
@@ -0,0 +1,15 @@
+class random;
+  rand bit[3:0] addr;
+  rand bit[3:0] data;
+  constraint c{addr inside{[0:10]};data inside{[0:12]};}
+endclass
+
+module tb;
+  initial begin
+    random r=new();
+    repeat(3)begin
+      r.randomize();
+      $display("addr=%0d,data=%0d",r.addr,r.data);
+    end
+  end
+endmodule
