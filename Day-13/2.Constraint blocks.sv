bd62d5479f86a038b301dd5a3eb632d33230211a
Murugesh06 <murugeshrajrp@gmail.com>
2025-11-04 10:31:50 +0530

    Create 2.Constraint blocks.sv

---
+++ Day-13/2.Constraint blocks.sv
@@ -0,0 +1,17 @@
+class random;
+  rand bit[2:0]addr1;
+  randc bit[2:0]addr2;
+  constraint c1{addr1>5;}
+  constraint c2;
+endclass
+constraint random::c2{addr2<5;}
+module tb;
+  random r;
+  initial begin
+    r=new();
+    repeat(10)begin
+    	r.randomize();
+    	$display("addr1=%0d,addr2=%0d",r.addr1,r.addr2);
+    end
+  end
+endmodule
